8c8096234286c6a40222840fcf7ee236320556b5
Sungyong Son <space010127@gmail.com>
2018-08-27 23:42:02 +0900

    [Update] Prepare Develop

---
+++ app/src/main/java/com/hanadal/dooson/hanadal/WriteMyDiaryActivity.java
@@ -0,0 +1,13 @@
+package com.hanadal.dooson.hanadal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WriteMyDiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_my_diary);
+    }
+}
