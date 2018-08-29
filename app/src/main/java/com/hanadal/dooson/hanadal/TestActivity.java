6d83e7eed5bc2a84dd4e5d26f5839afcd839d802
Sungyong Son <space010127@gmail.com>
2018-08-29 20:30:41 +0900

    [Update] Setting Data Binding

--- app/src/main/java/com/hanadal/dooson/hanadal/TestActivity.java
+++ app/src/main/java/com/hanadal/dooson/hanadal/TestActivity.java
@@ -1,24 +1,27 @@
 package com.hanadal.dooson.hanadal;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
 
 import com.google.gson.JsonArray;
 import com.hanadal.dooson.hanadal.connect.Connector;
 import com.hanadal.dooson.hanadal.connect.Res;
 import com.hanadal.dooson.hanadal.util.UtilClass;
+import com.hanadal.dooson.hanadal.databinding.*;
+
 
 public class TestActivity extends AppCompatActivity {
-    TextView textView;
+    ActivityTestBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
 
-        textView = findViewById(R.id.text_test);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_test);
+        binding.setActivity(this);
 
         UtilClass.Toast(getApplicationContext(), "Test");
 
@@ -29,9 +32,13 @@ public class TestActivity extends AppCompatActivity {
                 Log.d("**TEST**", String.valueOf(code));
                 if(code == 200){
                     Log.d("**TEST**", body.toString());
-                    textView.setText(body.toString());
+                    binding.textTest.setText(body.toString());
                 }
             }
         });
+    }
+
+    public void onButtonClick(View view){
+        UtilClass.Toast(getApplicationContext(), "Test");
     }
 }
