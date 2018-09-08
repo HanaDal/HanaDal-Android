1c2f9bb064f0c6ac7f76e4f417f7ad47df75c9cd
Sungyong Son <space010127@gmail.com>
2018-09-08 23:26:59 +0900

    [Update] Add Splash Activity Animation

--- app/src/main/java/com/hanadal/dooson/hanadal/ui/splash/SplashActivity.java
+++ app/src/main/java/com/hanadal/dooson/hanadal/ui/splash/SplashActivity.java
@@ -1,20 +1,49 @@
 package com.hanadal.dooson.hanadal.ui.splash;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 
 import com.hanadal.dooson.hanadal.R;
+import com.hanadal.dooson.hanadal.databinding.ActivitySplashBinding;
 import com.hanadal.dooson.hanadal.ui.sign.SignActivity;
 
 public class SplashActivity extends AppCompatActivity{
+    ActivitySplashBinding binding;
+    boolean ani = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+
+        AnimationSet set1 = new AnimationSet(true);
+
+        Animation anim2 = AnimationUtils.loadAnimation
+                (getApplicationContext(), R.anim.alpha_anim);
+
+        binding.star.startAnimation(anim2);
+
+        Animation anim3 = AnimationUtils.loadAnimation
+                (getApplicationContext(), R.anim.alpha_anim_two);
+
+        binding.title.setAnimation(anim3);
+        binding.subTitle.setAnimation(anim3);
+
+        set1.addAnimation(anim3);
+        set1.startNow();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -22,6 +51,20 @@ public class SplashActivity extends AppCompatActivity{
                 startActivity(new Intent(getApplicationContext(), SignActivity.class));
                 finish();
             }
-        }, 1500);
+        }, 3000);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if(!ani) {
+            TranslateAnimation anim = new TranslateAnimation
+                    (0,   // fromXDelta
+                            0,  // toXDelta
+                            binding.moon.getY(),    // fromYDelta
+                            0);// toYDelta
+            anim.setDuration(2500);
+            binding.moon.setAnimation(anim);
+            ani = true;
+        }
     }
 }
